4ec2c787ec08d91daa84e515072d147c167dec7a
x0rtex <s00253368@atu.ie>
2026-02-12 01:43:32 +0000

    Add project files.

---
+++ B4Budget/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace B4Budget
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ B4Budget/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace B4Budget
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
